a20a659c7e2f3cdda38a35cfe0b1ff1dfb6fd7b3
Jake <mail@jakepearse.com>
2014-10-12 10:36:11 +0100

    added material for corpus

---
+++ concept_examples/hello.erl
@@ -0,0 +1,23 @@
+-module(hello).
+-export([start/0]).
+
+start() ->
+    spawn(fun () -> {ok, Sock} = gen_tcp:listen(8000, [{active, false}]), 
+                    loop(Sock) end).
+
+loop(Sock) ->
+    {ok, Conn} = gen_tcp:accept(Sock),
+    Handler = spawn(fun () -> handle(Conn) end),
+    gen_tcp:controlling_process(Conn, Handler),
+    loop(Sock).
+
+handle(Conn) ->
+    gen_tcp:send(Conn, response("Hello World from erlang")),
+    gen_tcp:close(Conn).
+
+response(Str) ->
+    B = iolist_to_binary(Str),
+    iolist_to_binary(
+      io_lib:fwrite(
+         "HTTP/1.0 200 OK\nContent-Type: text/html\nContent-Length: ~p\n\n~s",
+         [size(B), B])).
--- concept_examples/swarmer.erl
+++ concept_examples/swarmer.erl
@@ -1,8 +1,8 @@
--module(hello).
--export([start/1]).
+-module(swarmer).
+-export([start/0]).
 
-start(Port) ->
-    spawn(fun () -> {ok, Sock} = gen_tcp:listen(Port, [{active, false}]), 
+start() ->
+    spawn(fun () -> {ok, Sock} = gen_tcp:listen(8000, [{active, false}]), 
                     loop(Sock) end).
 
 loop(Sock) ->
@@ -12,8 +12,13 @@ loop(Sock) ->
     loop(Sock).
 
 handle(Conn) ->
-    gen_tcp:send(Conn, response("Hello World")),
+    gen_tcp:send(Conn, response(nums())),
+    gen_tcp:close(Conn);
+
+handle(Conn,X) ->
+    gen_tcp:send(Conn, response(nums(X))),
     gen_tcp:close(Conn).
+
 
 response(Str) ->
     B = iolist_to_binary(Str),
@@ -21,3 +26,7 @@ response(Str) ->
       io_lib:fwrite(
          "HTTP/1.0 200 OK\nContent-Type: text/html\nContent-Length: ~p\n\n~s",
          [size(B), B])).
+
+nums() -> nums({random:uniform(50),random:uniform(50)});
+nums(X) ->
+{A,B} = X -> io:format("~d,~d",[A,B]).
