9caa5a7556488594c51afe1c3578f2ada3170a85
Joe Mitchard <jm710@kent.ac.uk>
2014-10-31 11:41:59 +0000

    added skeleton for gen_servers to concept_examples

---
+++ swarmer/src/tile.erl
@@ -0,0 +1,57 @@
+-module(tile).
+-author("Joe Mitchard jm710@kent.ac.uk").
+
+-behaviour(gen_server).
+
+%%%% API
+-export([start_link/0]).
+
+%%%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state,{}).
+
+%%%%%%=============================================================================
+%%%%%% API
+%%%%%%=============================================================================
+
+%%%%------------------------------------------------------------------------------
+%%%% @doc
+%%%% Start the server.
+%%%% @end
+%%%%------------------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%%%%=============================================================================
+%%%%%% gen_server Callbacks
+%%%%%%=============================================================================
+
+init([]) ->
+    {ok, #state{}}.
+
+handle_call(Request, From, State) ->
+    {stop, unexpected_call, {undefined, Request}, State}.
+
+handle_cast(Request, State) ->
+    {stop, unexpected_cast, State}.
+
+handle_info(Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%%%%=============================================================================
+%%%%%% Internal Functions
+%%%%%%=============================================================================
