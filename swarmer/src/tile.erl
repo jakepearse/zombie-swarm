8d35da8cdecd4561782c13a9b695e73aa46aa0f9
Joe Mitchard <Joe jm710@kent.ac.uk>
2014-11-04 17:43:06 +0000

    Semi functional tile module. Work in progress

--- swarmer/src/tile.erl
+++ swarmer/src/tile.erl
@@ -14,9 +14,14 @@
          terminate/2,
          code_change/3]).
 
+%%%% tile functions
+-export([get_population/1,
+        summon_entity/2]).
+
 -define(SERVER, ?MODULE).
 
--record(state,{}).
+%%%% zombieList : a list of all the Zombies on the current tile
+-record(tile_state,{entityList=[]}).
 
 %%%%%%=============================================================================
 %%%%%% API
@@ -35,13 +40,19 @@ start_link() ->
 %%%%%%=============================================================================
 
 init([]) ->
-    {ok, #state{}}.
+    {ok, #tile_state{}}.
 
-handle_call(Request, From, State) ->
-    {stop, unexpected_call, {undefined, Request}, State}.
+%%%%%% calls
+handle_call(get_population, _From, State) ->
+    {reply, State#tile_state.entityList, State}.
 
-handle_cast(Request, State) ->
-    {stop, unexpected_cast, State}.
+
+%%%%%% casts
+handle_cast({summon_entity, Entity}, State) when State#tile_state.entityList =/= [] ->
+    {noreply, State#tile_state{entityList=add_unique(State#tile_state.entityList,Entity)}};
+handle_cast({summon_entity, Entity}, State) when State#tile_state.entityList == [] ->
+    {noreply, State#tile_state{entityList=State#tile_state.entityList++[Entity]}}.
+
 
 handle_info(Info, State) ->
     {noreply, State}.
@@ -55,3 +66,33 @@ code_change(_OldVsn, State, _Extra) ->
 %%%%%%=============================================================================
 %%%%%% Internal Functions
 %%%%%%=============================================================================
+
+%%%%%% calls
+get_population(Pid) ->
+    gen_server:call(Pid, get_population).
+
+%%%%%% casts
+summon_entity(Pid, Entity) ->
+    gen_server:cast(Pid, {summon_entity, Entity}).
+
+
+%%%%%% functions
+check_list([],_) -> false;
+check_list({E1,{X1,Y1}},[{_,{X2,Y2}}|Xs]) ->
+    if (X1=/=X2) or (Y1=/=Y2) ->
+        check_list({E1,{X1,Y1}}, Xs);
+        true -> true
+    end.
+
+add_unique(List,Entity) ->
+    case check_list(List, Entity) of
+        false ->
+            List++[Entity];
+        true ->
+            {E,{X,Y}} = Entity,
+            add_unique(List,{E,{X+1,Y+1}})
+    end.
+
+
+% types and specs
+% translate list to dicts
