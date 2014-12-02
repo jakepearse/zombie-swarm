9d43b90aa5e978b6c67acf868d00abadb83136d8
rsjhales <rsjhales@gmail.com>
2014-12-02 16:49:43 +0000

    Improved zombie_fsm,added type

--- swarmer/src/zombie_fsm.erl
+++ swarmer/src/zombie_fsm.erl
@@ -1,27 +1,35 @@
 -module(zombie_fsm).
 -author("Robert Hales rsjh3@kent.ac.uk").
+-define(AIMLESS_STAY_COURSE, 8).
 -behaviour(gen_fsm).
 
 %gen_fsm implementation
 -export([code_change/4,handle_event/3,handle_sync_event/4,handle_info/3,init/1,terminate/3]).
 
--export([start_link/7,update/1,aimless/2,initial/2,aimless_search/2,active/2,active_search/2,chasing/2,chasing_search/2,calc_state/1,calc_aimlessbearing/4]).
--record(state, {id,tile,viewer,speed,bearing,x,y,timeoutz}).
+-export([start_link/7,aimless/2,initial/2,aimless_search/2,active/2,active_search/2,chasing/2,chasing_search/2,calc_state/1,calc_aimlessbearing/4,start/1,unpause/1,pause/2]).
+-record(state, {id,tile,viewer,speed,bearing,x,y,timeoutz,type}).
 
 start_link(X,Y,Tile,Viewer,Speed,Bearing,Timeout) -> 
 	gen_fsm:start_link(?MODULE,[X,Y,Tile,Viewer,Speed,Bearing,Timeout],[]).
 
 init([X,Y,Tile,Viewer,Speed,Bearing,Timeout]) ->
-	{ok,initial,#state{tile = Tile,viewer = Viewer, x = X, y = Y,speed=Speed, bearing=Bearing, timeoutz=Timeout},#state.timeoutz}.
+	random:seed(erlang:now()),
+	{ok,initial,#state{tile = Tile,viewer = Viewer, x = X, y = Y,speed=Speed, bearing=random:uniform(360), timeoutz=Timeout,type =zombie}}.
 
 %States of fsm.	
 
-initial(timeout,State) ->
-	{next_state,calc_state(aimless),State,300}.
+initial(start,State) ->
+	{next_state,calc_state(aimless),State,State#state.timeoutz}.
 	
 aimless(timeout,State) ->
-	random:seed(erlang:now()),
-	Bearing = random:uniform(360),
+	OldBearing = State#state.bearing,
+	StaySame = random:uniform(?AIMLESS_STAY_COURSE),
+	Bearing = case StaySame of
+		1 ->
+			random:uniform(360);
+		_ ->
+			OldBearing
+	end,
 	P = calc_aimlessbearing(Bearing,State#state.speed,State#state.x,State#state.y),
 	{NewX,NewY} = tile:update_entity(State#state.tile,{self(),{State#state.x,State#state.y}},P,Bearing,State#state.speed),
 	{next_state,aimless_search,State#state{x=NewX,y=NewY,bearing = Bearing},State#state.timeoutz}.
@@ -40,11 +48,13 @@ chasing(timeout,State) ->
 
 chasing_search(timeout,State) ->
 	{next_state,calc_state(chasing),State,State#state.timeoutz}.
-
+pause(unpause,State) ->
+	{next_state,aimless,State,State#state.timeoutz}.
 %Events for fsm.	
-update([Newx,Newy,Pid]) ->
-	gen_fsm:send_event(self(),{update,[Newx,Newy,Pid]}).
-
+start(Pid) ->
+	gen_fsm:send_event(Pid,start).
+unpause(Pid) -> 
+	gen_fsm:send_event(Pid,unpause).
 calc_state(_Current_state) ->
 	aimless.
 
