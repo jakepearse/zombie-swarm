dbf10fda05abb62252d0be8edabcd7d29d9c7d85
Joe Mitchard <joemitchard92@gmail.com>
2015-02-04 15:58:13 +0000

    Added requirements specification (and an updated version - such project managment)
    wow

--- documents/make_toc.py
+++ documents/make_toc.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python2
 import os
-print "<!DOCTYPE HTML><html><head></head><body><ol>\n"
+print "<!DOCTYPE HTML><html><head></head><body>\n"
+print "<table>\n"
 for dirname, dirnames, filenames in os.walk('.'):
     for filename in filenames:
-        if filename[:3]=="pdf":
-            print "<li><a href='"+os.path.join(dirname, filename)+"'>"+os.path.join(dirname, filename).strip()[2:]+"</li>\n"
+        if filename.strip()[-3:]=="pdf":
+            print "<tr><td></td><td></td><td><a href='"+os.path.join(dirname, filename)+"'>"+os.path.join(dirname, filename).strip()[2:]+"</td><td></td><td></td></tr>\n"
     # editing the 'dirnames' list will stop os.walk() from recursing into there.
     if '.git' in dirnames:
         # don't go into any .git directories.
         dirnames.remove('.git')
-print "</ol></body></html>"
+    if 'tex' in dirnames:
+     	dirnames.remove('tex')
+print "</table>"
+print "</body></html>"
