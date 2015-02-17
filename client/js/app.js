00d0898d67499edd5d59b546d4ca39e8e727b4dd
jake <j.t.pearse@gmail.com>
2015-02-17 22:27:40 +0000

    investigasting a problem with enviroment timeing out on any gen-server call

--- client/js/app.js
+++ client/js/app.js
@@ -1,11 +1,48 @@
 (function(){
   var app = angular.module('swarm',[]);
+  
+
+
 
   app.controller('SwarmController', function(){
+    
   
   });
   app.controller('PanelController', function(){
+  
+  function setup_grid(arrity,tileSize,gridScale) {
+  arrity = parseInt(arrity);
+  tileSize = parseInt(tileSize);
+  gridScale = parseInt(gridScale);
+  var list =[];
+  for (var i = 0; i <= arrity; i++) {
+    list.push(i*tileSize);
+  };
+  draw_hlines(arrity,tileSize,gridScale,list);
+  draw_vlines(arrity,tileSize,gridScale,list);
+};
+    
+    // compile a JSON object
+    var json = JSON.stringify({"type":"setup","arrity":arrity});
+    
+    //establish a socket
+    var socket= new WebSocket('ws://localhost:8080/websocket');
+    
+    // the onopen evnt hook triggers this
+    socket.onopen = function() {
+    socket.send(json);
+    // this is the event of a reply coming back
+    socket.onmessage = function(evt) {
+      // data from the reply
+      var data = JSON.parse(evt.data);
+      console.log(evt);
       
+      setup_grid(arrity,size,gridScale);
+      
+      
+    };
+  };
+  
   });
   
 
@@ -21,9 +58,12 @@
       swarmSize:50,
       popSize:50
     };
-	
-	var arrity = saneDefaultGrid.arrity;
-	var gridScale = saneDefaultGrid.gridScale;
-	var swarmSize = saneDefaultGrid.swamSize;
-	var popSize = saneDefaultGrid.popSize;
+
+  //var arrity = saneDefaultGrid.arrity;
+  //var gridScale = saneDefaultGrid.gridScale;
+  //var swarmSize = saneDefaultGrid.swamSize;
+  //var popSize = saneDefaultGrid.popSize;
+  
+
+  
 })();
