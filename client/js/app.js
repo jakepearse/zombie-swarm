b7dd2f5eb52c08f6154ae30ad85aaeb4673e0b75
jake <j.t.pearse@gmail.com>
2015-02-23 23:16:27 +0000

    well it's still not quite right but somethings are working

--- client/js/app.js
+++ client/js/app.js
@@ -15,67 +15,58 @@
     $scope.gridScale = 4;
     $scope.swarmSize = 50;
     $scope.popSize = 20;
-    var socket= new WebSocket('ws://localhost:8080/websocket');
-
+    socket = new WebSocket('ws://localhost:8080/websocket');
+    var json = JSON.stringify({"type":"setup","arrity":$scope.arrity,"swarmSize":$scope.swarmSize,"popSize":$scope.popSize});
+    $scope.inspectList = [];
+    socket.onopen = function() {
+      socket.send(json);
+      // this is in the event of a reply coming back
+      socket.onmessage = function(evt) {
+      // parse the data from the reply json object
+      var data = JSON.parse(evt.data);
+      // update the grid visulaisation
+      // draw the grid
+      var gridInfo = data.pop();
+      setup_grid(gridInfo.rows,gridInfo.tileSize,$scope.gridScale);
+      draw_circles(data,$scope.gridScale,$scope);
+      };
+    };
+    
+    // Yes it's a bit repetitive
     $scope.update = function() {
     d3.select("svg").html("");
-    var json = JSON.stringify({"type":"setup","arrity":$scope.arrity,"swarmSize":$scope.swarmSize,"popSize":$scope.popSize});
-    
+    var new_json = JSON.stringify({"type":"setup","arrity":$scope.arrity,"swarmSize":$scope.swarmSize,"popSize":$scope.popSize});
+    if (socket.readyState != 0) {
+    socket.send(new_json);
+  };
+    socket.onmessage = function(evt) {
     // the onopen event hook triggers this
-    socket.onopen = function() {
-		socket.send(json);
-		// this is in the event of a reply coming back
-		socket.onmessage = function(evt) {
-			// parse the data from the reply json object
-			var data = JSON.parse(evt.data);;
-			// update the grid visulaisation
-			
-			// draw the grid
-			var gridInfo = data.pop();
-			setup_grid(gridInfo.rows,gridInfo.tileSize,$scope.gridScale);
-			draw_circles(data,$scope.gridScale);
-			};
+      var data = JSON.parse(evt.data);;
+      // update the grid visulaisation
+      // draw the grid
+      var gridInfo = data.pop();
+      setup_grid(gridInfo.rows,gridInfo.tileSize,$scope.gridScale);
+      draw_circles(data,$scope.gridScale,$scope);
+      };
 		};
-	};
 	
-	
-	//$scope.updateZoom = function() {
-		////setup_grid($scope.arrity,50,$scope.gridScale);
-		//$scope.updateGrid();
-		//};
-		
-	//$scope.updateSwarm = function() {
-		//var swarm_json = JSON.stringify({"type":"swarm","size":$scope.swarmSize});
-		//socket.send(swarm_json);
-		//socket.onmessage = function(sw) {
-		//// draw the swarm 
-          //swarm_data = JSON.parse(sw.data);
-          ////console.log(swarm_data);
-          //draw_circles(swarm_data,$scope.gridScale);
-        //};    
-	//};
-		
+
 	$scope.start = function() {
-		var inspector = angular.element("#inspector");
-		console.log(inspector);
+		$scope.inspector = angular.element("#inspector");
 		var startjson = JSON.stringify({"type":"start"});
 		socket.send(startjson);
 		socket.onmessage = function (x) {;}
-		inspector.value ="";
+		//inspector.value ="";
 		var dummy_json = JSON.stringify({"type":"report"});
 		//This sends the "update" message to the socket every 1000ms
 		// and updates the circles with the recived data
 		setInterval(function() {doUpdate()},300);
 		function doUpdate() {
-			var inspectList =[];
 			socket.send(dummy_json);
 			socket.onmessage = function(evt) {
 				var report_json = JSON.parse(evt.data);
 				update_circles(report_json,$scope.gridScale,$scope.swarmSize);
-				for (var i = 0; i<inspectList.length; i++) {
-					var element = inspectList[i];
-      inspector.value = "id: "+element.__data__.id+", Pos: "+element.__data__.x+","+element.__data__.y+","+element.__data__.viewer+"\n" + inspector.value;
-    }
+        
     };
   };
 };
