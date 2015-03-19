3eb3c0bde7c6c9d5b24084da156045f6928a253c
Jake <j.t.pearse@gmail.com>
2015-03-19 15:29:51 +0000

    los implemeneted

--- swarmer/src/los.erl
+++ swarmer/src/los.erl
@@ -12,19 +12,19 @@ findline(X1,Y1,X2,Y2,_Olist) when (abs( X2-X1 ) < 5) and (abs( Y2-Y1 )) < 5 ->
   true;
   
 % Horizontal line to the right, rise = 0
-findline(X1,Y1,X2,Y2,Olist) when ((Y1 div 5)-(Y2 div 5)==0) and (X1 < X2) ->
-  filterlist([{CheckX,CheckY} || CheckX <- lists:seq(X1 div 5,X2 div 5,5), CheckY <- [Y1 div 5]],Olist);
+findline(X1,Y1,X2,Y2,Olist) when ((Y2 div 5)-(Y1 div 5)==0) and (X1 < X2) ->
+  filterlist([{CheckX,CheckY} || CheckX <- lists:seq(X1 div 5,X2 div 5,1), CheckY <- [Y1 div 5]],Olist);
 % Horizontal line to the left, rise 0
-findline(X1,Y1,X2,Y2,Olist) when ((Y1 div 5)-(Y2 div 5)==0) and (X1 > X2) ->
-  filterlist([{CheckX,CheckY} || CheckX <- lists:seq(X1 div 5,X2 div 5,-5), CheckY <- [Y1 div 5]],Olist);
+findline(X1,Y1,X2,Y2,Olist) when ((Y2 div 5)-(Y1 div 5)==0) and (X1 > X2) ->
+  filterlist([{CheckX,CheckY} || CheckX <- lists:seq(X1 div 5,X2 div 5,-1), CheckY <- [Y1 div 5]],Olist);
 
 % Vertical Line down, run = 0
 findline(X1,Y1,X2,Y2,Olist) when ( (X2 div 5)-(X1 div 5) == 0 ) and (Y1 < Y2) -> 
-  filterlist([{CheckX,CheckY} || CheckX <- [X1 div 5], CheckY <- lists:seq(Y1 div 5, Y2 div 5, 5)],Olist);
+  filterlist([{CheckX,CheckY} || CheckX <- [X1 div 5], CheckY <- lists:seq(Y1 div 5, Y2 div 5)],Olist);
 
 % vertical line up, run =0
 findline(X1,Y1,X2,Y2,Olist)  when ( (X2 div 5) - (X1 div 5) ==0 ) and (Y1 > Y2) -> 
-  filterlist([{CheckX,CheckY} || CheckX <- [X1 div 5], CheckY <- lists:seq(Y1 div 5,Y2 div 5,-5)],Olist);
+  filterlist([{CheckX,CheckY} || CheckX <- [X1 div 5], CheckY <- lists:seq(Y1 div 5,Y2 div 5,-1)],Olist);
 
 % slope - find Y=MX+C
 findline(X1,Y1,X2,Y2,Olist) -> 
@@ -35,23 +35,46 @@ findline(X1,Y1,X2,Y2,Olist) ->
     true -> true;
     false ->
   %error_logger:error_report({"XX,YY",{XX,YY}}),
-      M = YY/XX,
+      M = YY / XX,
       %error_logger:error_report({"M",M}),
       % Y = MX+C
       % Y-C = MX
       % -C = MX-Y
-      C = -( (M * (X1 div 5) ) - (Y1 div 5) ),
+      C = ((Y1 div 5) - (M * (X1 div 5) ) ),
       %error_logger:error_report({"C",C}),
       findline(X1 div 5,X2 div 5,Y1 div 5,Y2 div 5,M,C,Olist) end.
 
-findline(X1,X2,_Y1,_Y2,M,C,Olist) when X1 < X2 ->
-  F=[{CheckX,CheckY} || CheckX <- lists:seq(X1+1,X2), CheckY <- [round(M*CheckX+C)]],
-  %error_logger:error_report({"F",F}),
+findline(X1,X2,Y1,Y2,M,C,Olist) when X1 < X2 ->
+  Xs = [{CheckX,CheckY} || CheckX <- lists:seq(X1,X2), CheckY <- [M*CheckX+C]],
+  case Y1<Y2 of
+  true ->
+    %Xs = [{CheckX,CheckY} || CheckX <- lists:seq(X1,X2), CheckY <- [round(M*CheckX+C)]],
+    Ys = [{CheckX,CheckY} || CheckY <- lists:seq(Y1,Y2), CheckX <- [(CheckY-C) / M]];
+    %error_logger:error_report({"Xs",Xs,"Ys",Ys});
+    
+  false ->
+    Ys=[{CheckX,CheckY} || CheckY <- lists:seq(Y1,Y2,-1), CheckX <- [(CheckY-C) / M]]
+  end,
+  
+  F=sets:to_list(sets:from_list(Xs++Ys)),
+  %error_logger:error_report({"F1",F}),
+  %error_logger:error_report({"Xs",Xs,"Ys",Ys}),
   filterlist(F,Olist);
   
-findline(X1,X2,_Y1,_Y2,M,C,Olist) when X1 > X2 ->
-  F= [{CheckX,CheckY} || CheckX <- lists:seq(X1-1,X2,-1), CheckY <- [round(M*CheckX+C)]],
-  %error_logger:error_report({"F",F}),
+findline(X1,X2,Y1,Y2,M,C,Olist) when X1 > X2 ->
+  case Y1<Y2 of
+  true ->
+    Xs= [{CheckXa,CheckYa} || CheckXa <- lists:seq(X1,X2,-1), CheckYa <- [M*CheckXa+C]],
+    Ys = [{CheckXb,CheckYb} || CheckYb <- lists:seq(Y1,Y2), CheckXb <- [(CheckYb-C) / M]];
+   
+  false ->
+        Xs= [{CheckXc,CheckYc} || CheckXc <- lists:seq(X1-1,X2,-1), CheckYc <- [M*CheckXc+C]],
+        Ys= [{CheckXd,CheckYd} || CheckYd <- lists:seq(Y1,Y2,-1), CheckXd <- [(CheckYd-C) / M]]
+        
+  end,
+  F=sets:to_list(sets:from_list(Xs++Ys)),
+  %error_logger:error_report({"F2",F}),
+   %error_logger:error_report({"Xs",Xs,"Ys",Ys}),
   filterlist(F,Olist).
 %every element of checklist is the atom false
 filterlist([],_Olist) ->
@@ -59,11 +82,13 @@ filterlist([],_Olist) ->
   
 filterlist(CheckList,Olist) ->
   [H|T] = CheckList,
+  {X,Y} = H,
+  Reasons = {trunc(X),trunc(Y)},
   %error_logger:error_report([{"head",H},Olist]),
-  case lists:member(H,Olist) of
+  case lists:member(Reasons,Olist) of
     true -> false;
     false ->
-    %error_logger:error_report([{"H",H},Olist]),
+   % error_logger:error_report([{"H",H},Olist]),
       filterlist(T,Olist)
     end.
   
